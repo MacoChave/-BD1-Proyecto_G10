56a9b1c33e5d642eea69c981ea791afe290b4292
johnnybro <jbravozam@gmail.com>
2021-04-11 19:54:35 -0600

    Archivos de carga 2

---
+++ Archivos de Carga/Carrera.sql
@@ -0,0 +1,10 @@
+insert into Carrera (Carrera, Nombre) values(01, 'Ingenieria Civil');
+insert into Carrera (Carrera, Nombre) values(02, 'Ingenieria Quimica');
+insert into Carrera (Carrera, Nombre) values(03, 'Ingenieria Mecanica');
+insert into Carrera (Carrera, Nombre) values(04, 'Ingenieria Electrica');
+insert into Carrera (Carrera, Nombre) values(05, 'Ingenieria Insdustrial');
+insert into Carrera (Carrera, Nombre) values(06, 'Ingenieria Mecanica Electrica');
+insert into Carrera (Carrera, Nombre) values(07, 'Ingenieria Mecanica Insdustrial');
+insert into Carrera (Carrera, Nombre) values(09, 'Ingenieria en Ciencia y Sistemas');
+insert into Carrera (Carrera, Nombre) values(13, 'Ingenieria Electronica');
+insert into Carrera (Carrera, Nombre) values(35, 'Ingenieria Ambiental');
